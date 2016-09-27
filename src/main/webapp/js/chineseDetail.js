e4125c486ec32c21559a5462b974ee4f3ab9619e
Zmingqin <zhangmingqinemail@163.com>
2016-09-27 18:45:56 +0800

    options

--- src/main/webapp/js/chineseDetail.js
+++ src/main/webapp/js/chineseDetail.js
@@ -90,9 +90,131 @@
                         data: data.comments,
                         containerId: 'comments'
                     })
+
+                    let findProblem = function(problemId) {
+                        let problem = null
+                        for (let i = 0; i < data.problems.length; ++i) {
+                            if (data.problems[i].id == problemId) {
+                                problem = data.problems[i]
+                                break
+                            }
+                        }
+                        return problem
+                    }
+
+                    let getIndex = function(content) {
+                        let index = -1
+                        switch (content) {
+                            case 'A':
+                                index = 0
+                                break
+                            case 'B':
+                                index = 1
+                                break
+                            case 'C':
+                                index = 2
+                                break
+                            case 'D':
+                                index = 3
+                                break
+                            default:
+                                break
+                        }
+                        return index
+                    }
+
+                    let compareAnswer = function(index, standardAnswers) {
+                        let finded = false
+                        for (let j = 0; j < standardAnswers.length; ++j) {
+                            if (index == standardAnswers[j].name) {
+                                finded = true
+                                break
+                            }
+                        }
+                        return finded
+                    }
+
+
+                    let problemContainer = document.getElementById('problem')
+                    problemContainer.addEventListener('click', function (e) {
+                        //e.currentTarget == problemContainer
+                        let clickedElement = e.target
+                        let trueImage = document.createElement('img')
+                        trueImage.setAttribute('class', 'daan_error')
+                        trueImage.setAttribute('src', 'img/true.png')
+                        trueImage.setAttribute('alt', '')
+
+                        let falseImage = document.createElement('img')
+                        falseImage.setAttribute('class', 'daan_error')
+                        falseImage.setAttribute('src', 'img/error.png')
+                        falseImage.setAttribute('alt', '')
+
+                        if (clickedElement.hasClass('daan_quan')) { // == [class="daan_quan"]
+                            let problemId = clickedElement.parentNode.parentNode.dataset.id
+                            let problem = findProblem(problemId)
+                            if (problem) {
+                                let index = getIndex(clickedElement.textContent)
+                                let r = compareAnswer(index, problem.ProblemStandardAnswer)
+                                if (r) {
+                                    clickedElement.parentNode.addClass('daanLi_true')
+                                    clickedElement.innerHTML = ''
+                                    clickedElement.appendChild(trueImage)
+                                } else {
+                                    clickedElement.parentNode.addClass('daanLi_error')
+                                    clickedElement.innerHTML = ''
+                                    clickedElement.appendChild(falseImage)
+                                }
+                            }
+                        }
+                    }, false)
+
+                    //POST /api/problems/{id}/answers
+                   // answer-records
+                    //
+                    //[1,3,4]
+                    $.ajax({
+                        type: "post",
+                        url: 'api/problems/' + id + '/answers',
+                        dataType: 'json',
+                        async: false,
+                        success: function (data) {
+                            alert(JSON.stringify(data))
+                        }
+                    })
+
+                    // $('.mld_daanLi').each(function(index, el) {
+                    //     $(this).click(function() {
+                    //         if($(this).index()==1){
+                    //             $(this).addClass('daanLi_true')
+                    //             $(this).children('span').html('<img class="daan_error" src="img/true.png" alt="">')
+                    //         }else{
+                    //             $(this).addClass('daanLi_error')
+                    //             $(this).children('span').html('<img class="daan_error" src="img/error.png" alt="">')
+                    //         }
+                    //     })
+                    // })
                 }
             })
         }
     })
-
 })
+    // {"comments":[],
+    //     "contents":[],
+    //     "interaction":{"likeCount":0,"readCount":76},
+    //     "title":"是神农",
+    //     "quotes":[],
+    //     "problems":[
+    //         {"options":[{"id":96647314735105,"problemId":96647314669568,"name":"11"},{"id":96647314735106,"problemId":96647314669568,"name":"22"},{"id":96647314735107,"problemId":96647314669568,"name":"33"},{"id":96647314735108,"problemId":96647314669568,"name":"44"}],
+    //             "id":96647314669568,
+    //             "type":"单选题",
+    //             "title":"1.神",
+    //             "ProblemStandardAnswer":[{"id":96647314735104,"problemId":96647314669568,"name":0}]
+    //         },
+    //        {"options":[{"id":96647316176898,"problemId":96647316176896,"name":"肃肃"},{"id":96647316176899,"problemId":96647316176896,"name":"物外"},{"id":96647316242432,"problemId":96647316176896,"name":"啊啊"},{"id":96647316242433,"problemId":96647316176896,"name":"吖吖"}],
+    //            "id":96647316176896,
+    //            "type":"单选题",
+    //            "title":"2.我",
+    //            "ProblemStandardAnswer":[{"id":96647316176897,"problemId":96647316176896,"name":1}]
+    //        }
+    //        ]
+    // }
