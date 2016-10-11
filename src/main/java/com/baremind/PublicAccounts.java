cd0a50f5505f98c7df2f8698b3a029fd06dcf85e
DuanShengpeng <duanshp@chineseall.com>
2016-10-11 16:02:22 +0800

    fix any empty field for user

--- src/main/java/com/baremind/PublicAccounts.java
+++ src/main/java/com/baremind/PublicAccounts.java
@@ -418,6 +418,126 @@ public class PublicAccounts {
         return result;
     }
 
+    public static class WechatUserInfo {
+        private int subscribe;
+        private String openid;
+        private String nickname;
+        private Long sex;
+        private String language;
+        private String city;
+        private String province;
+        private String country;
+        private String headimgurl;
+        private int subscribe_time;
+        private String unionid;
+        private String remark;
+        private int groupid;
+
+        public int getSubscribe() {
+            return subscribe;
+        }
+
+        public void setSubscribe(int subscribe) {
+            this.subscribe = subscribe;
+        }
+
+        public String getOpenid() {
+            return openid;
+        }
+
+        public void setOpenid(String openid) {
+            this.openid = openid;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+        public Long getSex() {
+            return sex;
+        }
+
+        public void setSex(Long sex) {
+            this.sex = sex;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getProvince() {
+            return province;
+        }
+
+        public void setProvince(String province) {
+            this.province = province;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getHeadimgurl() {
+            return headimgurl;
+        }
+
+        public void setHeadimgurl(String headimgurl) {
+            this.headimgurl = headimgurl;
+        }
+
+        public int getSubscribe_time() {
+            return subscribe_time;
+        }
+
+        public void setSubscribe_time(int subscribe_time) {
+            this.subscribe_time = subscribe_time;
+        }
+
+        public String getUnionid() {
+            return unionid;
+        }
+
+        public void setUnionid(String unionid) {
+            this.unionid = unionid;
+        }
+
+        public String getRemark() {
+            return remark;
+        }
+
+        public void setRemark(String remark) {
+            this.remark = remark;
+        }
+
+        public int getGroupid() {
+            return groupid;
+        }
+
+        public void setGroupid(int groupid) {
+            this.groupid = groupid;
+        }
+    }
+
     public static WechatUserInfo getUserInfo(String openId) {
         // http请求方式: GET（请使用https协议）
         //https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
@@ -450,14 +570,22 @@ public class PublicAccounts {
         long userId = IdGenerator.getNewId();
         User user = new User();
         user.setId(userId);
-        user.setHead(userInfo.headimgurl);
+        if (userInfo.headimgurl == null) {
+            user.setHead("");
+        } else {
+            user.setHead(userInfo.headimgurl);
+        }
         if (userInfo.nickname == null) {
             user.setName("");
         } else {
             user.setName(userInfo.nickname);
         }
         //u.setLoginName(us.nickname);
-        user.setSex(userInfo.sex);
+        if (userInfo.sex == null) {
+            user.setSex(0l);
+        } else {
+            user.setSex(userInfo.sex);
+        }
         user.setCreateTime(now);
         user.setUpdateTime(now);
         user.setIsAdministrator(false);
@@ -1737,22 +1865,6 @@ public class PublicAccounts {
         public static class UserData {
             private String[] openid;
         }
-    }
-
-    public static class WechatUserInfo {
-        private int subscribe;
-        private String openid;
-        private String nickname;
-        private Long sex;
-        private String language;
-        private String city;
-        private String province;
-        private String country;
-        private String headimgurl;
-        private int subscribe_time;
-        private String unionid;
-        private String remark;
-        private int groupid;
     }
 
     // 关注/取消关注事件
