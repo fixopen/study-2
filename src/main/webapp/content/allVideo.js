b98a0edb162af9932e43bd5c85972c41b7b6451c
hss <hss@gmail.com>
2016-10-27 11:30:27 +0800

    git

--- src/main/webapp/content/allVideo.js
+++ src/main/webapp/content/allVideo.js
@@ -9,18 +9,22 @@ function subjectTab(){ //课程选项卡
 
 function curseDetailTap(){ //点击“课程详情”
 	$(".course-detail-btn").each(function(index, el) {
-		$(this).click(function() {
-			if($(this).children("img").is(":visible")){
-				$(".course-detail-btn img").show();
-				$(".course-detail").hide();
-				$(this).children("img").hide();
-				$(".course-detail").eq(index).show();
-			}else{
-				$(".course-detail-btn img").show();
-				$(".course-detail").hide();
-				$(this).children("img").show();
-				$(".course-detail").eq(index).hide();
-			}
-		});
+		var item = $(this)[0]
+		if (!item.dataset.setclick) {
+			$(this).click(function() {
+				if($(this).children("img").is(":visible")){
+					$(".course-detail-btn img").show();
+					$(".course-detail").hide();
+					$(this).children("img").hide();
+					$(".course-detail").eq(index).show();
+				}else{
+					$(".course-detail-btn img").show();
+					$(".course-detail").hide();
+					$(this).children("img").show();
+					$(".course-detail").eq(index).hide();
+				}
+			});
+			item.dataset.setclick = true
+		}
 	});
 }
