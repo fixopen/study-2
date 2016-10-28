46c387e2ff73680356f9719e0149d35005e63ba2
Zmingqin <zhangmingqinemail@163.com>
2016-10-28 21:29:08 +0800

    new

--- src/main/webapp/js/chineseVolume.js
+++ src/main/webapp/js/chineseVolume.js
@@ -4,10 +4,7 @@ $(document).ready(function () {
 
     $.ajax({
         type: "get",
-        url: 'api/volumes?filter=' + JSON.stringify({
-            subjectId: 1, //parseInt(subjectId),
-            grade: 20
-        }),
+        url: 'api/subjects/1/low/volumes',
         success: function (volumesL) {
             // proc({
             //     templateId: 'volumes-template',
@@ -27,10 +24,7 @@ $(document).ready(function () {
 
     $.ajax({
         type: "get",
-        url: 'api/volumes?filter=' + JSON.stringify({
-            subjectId: 1,
-            grade: 21
-        }),
+        url: 'api/subjects/1/high/volumes',
         success: function (volumesH) {
             // proc({
             //     templateId: 'volumes-template',
