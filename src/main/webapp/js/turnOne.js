129a8e6f1d7e1bcf8faf047610103a0c49544e7c
Zmingqin <zhangmingqinemail@163.com>
2016-10-28 20:37:02 +0800

    new

--- src/main/webapp/js/turnOne.js
+++ src/main/webapp/js/turnOne.js
@@ -3,7 +3,7 @@ $('.table_li').eq(0).css({"box-shadow": "0rem 0rem 1.25rem #fe6569"});
 $('.table_li').eq(0).children('img').show();
 
 
-$('.table_li').eq(0).tap(function() {
+$('.table_li').eq(0).click(function() {
     $('.table_li').eq(0).children('img').show();
     $('.table_li').eq(1).children('img').hide();
     $(this).css({"box-shadow": "0rem 0rem 1.25rem #fe6569"});
@@ -12,7 +12,7 @@ $('.table_li').eq(0).tap(function() {
     $('.div0_two').hide();
 });
 
-$('.table_li').eq(1).tap(function() {
+$('.table_li').eq(1).click(function() {
     $('.table_li').eq(0).children('img').hide();
     $('.table_li').eq(1).children('img').show();
     $(this).css({"box-shadow": "0rem 0rem 1.25rem #23cbd1"});
