c022ba7976df33214842045cfa681347a2b871e9
DuanShengpeng <duanshp@chineseall.com>
2016-11-21 18:20:01 +0800

    fix compiler error

--- src/main/java/com/baremind/utils/Impl.java
+++ src/main/java/com/baremind/utils/Impl.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
