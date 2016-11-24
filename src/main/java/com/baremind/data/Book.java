2ef303e4e3e9a29647f49a0233d340b82881ddfc
DuanShengpeng <duanshp@chineseall.com>
2016-11-24 16:29:54 +0800

    merge

--- src/main/java/com/baremind/data/Book.java
+++ src/main/java/com/baremind/data/Book.java
@@ -1,9 +1,13 @@
 package com.baremind.data;
 
+import com.baremind.utils.JPAEntry;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by fixopen on 15/11/2016.
